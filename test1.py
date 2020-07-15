3e5d2aff5d0a7d0e33e104ec0f4697046722efcf
mrybalki <67759718+mrybalki@users.noreply.github.com>
2020-07-15 14:53:30 -0400

    Added analyzer menu
    added game analyzer mode, but analyzing functionality is not implemented yet.

--- test1.py
+++ test1.py
@@ -6,19 +6,24 @@ gameOn = True
 cmdDictionary = {"help": "helpCmd()",
                  "quit": "gameOn = False",
                  "rules": "rulesCmd()",
-                 "start game": "startCmd()",
+                 "start new game": "startCmd()",
                  "display board": "displayBoard(piece_positions)",
-                 "display empty board": "displayBoard()",
-                 "move": "attemptMove(coords,desiredCoords)",
                  "reset board": "piece_positions = setUpPieces()",
-                 "show PGN":"pgnCmd()"
+                 "show PGN": "pgnCmd()",
+                 "resign": "resignCmd()",
+                 "analyze": "analyzeCmd()"
+}
+
+#dictionary with user commands available in the game analyzer
+analyzeDictionary = {"help": "helpAnalyze()",
+                     "show PGN": cmdDictionary["show PGN"]
 }
 
 #print the current board
-def displayBoard(piece_positions=np.full((8,8)," ")): 
+def displayBoard(piece_positions=np.full((8,8)," "),playing=True): 
     board = createBackground() #creates background for the board
     emptyLine = "\n  |---|---|---|---|---|---|---|---|\n"
-    outputDisplay = ""
+    outputDisplay = "CURRENT GAME:" if playing else "GAME ANALYZER:"
     for i, row in enumerate(board, start=0):
         outputDisplay+=emptyLine
         outputDisplay+=str(8-i)+" "
@@ -63,12 +68,12 @@ def checkNormalPieceMoves(piecePositions,coords):
             if pieceColor == "r" and piecePositions[pieceY-1,pieceX+xOffset] == " " and pieceX+xOffset >= 0: #if this is a legal move for red and the legal move is on the board
                 emptyAdjacent.append((pieceY,pieceX,pieceY-1,pieceX+xOffset)) #append the piece and a possible location
         except: #a possible peice's location has a coordinate 8 (off the board)
-            continue #don't add anything to legal moves
+            print("checkerbot: this isn't a valid move.")
         try:
             if pieceColor == "b" and piecePositions[pieceY+1,pieceX+xOffset] == " " and pieceX+xOffset >= 0: #if this is a legal move for black and the legal move is on the board
                 emptyAdjacent.append((pieceY,pieceX,pieceY+1,pieceX+xOffset)) #append the piece and a possible location
         except: #a possible peice's location has a coordinate 8 (off the board)
-            continue #don't add anything to legal moves
+            print("checkerbot: this isn't a valid move.")
     return list(emptyAdjacent)
 #possible moves are stored in the following format:
 #piece's current Y, piece's current X, piece's possible new Y, piece's possible new X
@@ -105,8 +110,8 @@ def attemptMove(coords, desiredCoords):
     global gamePGN
     pieceY, pieceX = coords
     destY, destX = desiredCoords
-    if (gamePGN.count("\n")%2 == 0 and piece_positions[pieceY,pieceX].lower() == 'b') or (gamePGN.count("\n")%2 == 1 and piece_positions[pieceY,pieceX].lower() == 'r'):
-        print("chessbot: You cannot move this piece, it is not that player's turn.")
+    if (gamePGN.count(".")%2 == 0 and piece_positions[pieceY,pieceX].lower() == 'b') or (gamePGN.count(".")%2 == 1 and piece_positions[pieceY,pieceX].lower() == 'r'):
+        print("checkerbot: You cannot move this piece, it is not that player's turn.")
         return None
     if piece_positions[pieceY,pieceX] in ('r','b'):
         pMoves = checkNormalPieceMoves(piece_positions,coords)
@@ -118,8 +123,8 @@ def attemptMove(coords, desiredCoords):
             else:
                 gamePGN = updatePGN(gamePGN, pieceY,pieceX,destY,destX)
             displayBoard(piece_positions)
-        else:
-            print("chessbot: this isn't a valid move")
+        else: #this move cannot be made with the piece
+            print("checkerbot: this isn't a valid move.")
     elif piece_positions[pieceY,pieceX] in ('R','B'):
         pMoves = checkKingPieceMoves(piece_positions,coords)
         if (pieceY, pieceX, destY, destX) in pMoves:
@@ -127,8 +132,10 @@ def attemptMove(coords, desiredCoords):
             piece_positions[pieceY,pieceX] = " "
             displayBoard(piece_positions)
             gamePGN = updatePGN(gamePGN, pieceY,pieceX,destY,destX)
-        else:
-            print("chessbot: this isn't a valid move")
+        else: #this move cannot be made with the piece
+            print("checkerbot: this isn't a valid move.")
+    else: #empty tile is selected
+        print("checkerbot: this isn't a valid move.")
 
 #converts between user coordinates and array indices
 def convertCoords(coordStr,toIndices = True):
@@ -142,8 +149,7 @@ def convertCoords(coordStr,toIndices = True):
     elif not toIndices:
         output.append(8-int(coordStr[0]))
         output.append(chr(int(coordStr[1])+97))
-    return output[::-1]
-        
+    return output[::-1]      
 #a has an ascii value of 97, increments by 1 for following letters
 
 #updates the record of all moves played in a game
@@ -153,21 +159,20 @@ def updatePGN(PGN,pY,pX,dY,dX,flags=["move"]):
                 "king":'k'}
     pX, pY = convertCoords(str(pY)+str(pX),False)
     dX, dY = convertCoords(str(dY)+str(dX),False)
-    if PGN.count("\n") < 9: #add 0 for 1 digit number moves
+    if PGN.count(".") < 9: #add 0 for 1 digit number moves
         PGN+="0"
-    PGN+=str(PGN.count("\n")+1)+". " #add move number
-    PGN+='B ' if PGN.count("\n") %2 else 'R ' #add player
+    PGN+=str(PGN.count(".")+1)+"." #add move number
     PGN+=pX+str(pY)+dX+str(dY) #add coordinates
     for flag in flags:
       PGN+=flagDict[str(flag)] #add flags
-    return PGN+"\n"
+    return PGN+","
 
 #USER COMMANDS FROM HERE ON
 def helpCmd():
-    print("COMMANDS:\n    quit: closes game\n    rules: prints rules\n    start game: starts a game\n    display board: displays the checkers board\n    display empty board: displayes an empty chess board(temporary command)\n    reset board: resets the board")
+    print("checkerbot: commands...\n    quit: closes game\n    rules: prints rules\n    start new game: starts a game\n    display board: displays the checkers board\n    reset board: resets the board\n    show PGN: prints the current saved PGN\n    move __ __: moves a checkers piece after a game is started. enter the current coordinates of the piece first, and the desired coordinates of the piece second. (ex: move a3 b4)\n    paste __: pastes in a checkers PGN for later analysis. Make sure that the PGN is in the format (move number).(piece's old coordinate)(piece's new coordinate)(move flag), ... and so on with no spaces. (ex: 01.a3b4m,02.b6a5m ...). This command will restart any games in progress.\n    analyze: opens the game analyzer, using the current PGN")
 
 def rulesCmd():
-    print("chessbot: the rules for our checkers game should be here")
+    print("checkerbot: the rules for our checkers game should be here.")
 
 def startCmd():
     global piece_positions
@@ -175,23 +180,49 @@ def startCmd():
     displayBoard(piece_positions)
 
 def pgnCmd():
-    print(gamePGN)
+    print("checkerbot: the current PGN is\n    "+gamePGN)
+
+def resignCmd():
+    piece_positions = setUpPieces()
+    loser, winner = ("Red Player","Black Player") if gamePGN.count(".") % 2 == 0 else ("Black Player","Red Player")
+    print("checkerbot: "+loser+" resigns, "+winner+" wins!")
+
+def analyzeCmd():
+    if len(gamePGN) == 0:
+        print("checkerbot: no PGN to analyze. Play a game of checkers or use the paste command to analyze an existing PGN.")
+    else: #we have a PGN
+        analyzing = True
+        piece_positions = setUpPieces()
+        displayBoard(piece_positions,playing=False)
+        while analyzing:
+            userCmd = input("you: ")
+            if userCmd == "quit":
+                analyzing = False
+            else:
+                exec(analyzeDictionary[userCmd])
+        print("checkerbot: exiting analyzer...")
+
+def helpAnalyze():
+    print("checkerbot: analyzer commands...\n    quit: exits the analyzer\n    n: moves to the next move in the game\n    b: moves to the previous move in the game\n    show PGN: prints the entire PGN being analyzed")
+
 
 #checkOnePieceMoves(piece_positions,[2,1])
-print("chessbot: hello! welcome to PGSS checkers!")
-print("chessbot: for commands and rules, type \"help\"")
+print("checkerbot: hello! welcome to PGSS checkers!")
+print("checkerbot: for commands and rules, type \"help\".")
 while gameOn: #prompt user for input and execute user's commands until stopped
     #print(gameOn)
     userCmd = input("you: ")
-    #try:
-    if userCmd[:4] == "move": #if a move is being made
-        userCoords = convertCoords(userCmd[5:7])
-        userDestination = convertCoords(userCmd[8:10])
-        attemptMove(userCoords,userDestination)
-    elif userCmd[:5] == "paste": #if a PGN is being pasted
-        gamePGN = userCmD[5:]
-        print(gamePGN)
-    else:
-        exec(cmdDictionary[userCmd]) #execute user's command
-    #except:
-        #print("chessbot: "+userCmd+" isn't a valid command. If you are trying to display a board, make sure you have started a game")
+    try:
+        if userCmd[:4] == "move": #if a move is being made
+            userCoords = convertCoords(userCmd[5:7])
+            userDestination = convertCoords(userCmd[8:10])
+            attemptMove(userCoords,userDestination)
+        elif userCmd[:5] == "paste": #if a PGN is being pasted
+            gamePGN = userCmd[6:]
+            print("checkerbot: PGN recieved starting with "+gamePGN[:8]+"...")
+            piece_positions = setUpPieces()
+        else: #some other command is entered
+            exec(cmdDictionary[userCmd]) #execute user's command
+    except:
+        print("chessbot: "+userCmd+" isn't a valid command. If you are trying to display a board, make sure you have started a game")
+print("checkerbot: exiting PGSS chess...")
